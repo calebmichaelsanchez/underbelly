c09e3d33a7f8eab87004fa1788655da255855233
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2015-01-31 12:52:56 -0500

    Work on the footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,45 @@
+<footer>
+	<div class="tour">
+		<h2>Still not sure how getCoffee can help?</h2>
+		<a href="#" class="btn">Take the tour</a>
+	</div>
+	<hr>
+	<div class="navigation">
+		<div>
+			<h6>Product</h6>
+			<ul>
+				<li>Success Stories</li>
+				<li>Agenda Library</li>
+				<li>Terms</li>
+				<li>Pricing</li>
+				<li>Support</li>
+				<li>Sign In</li>
+			</ul>
+		</div>
+		<div>
+			<h6>Compnay</h6>
+			<ul>
+				<li>About Us</li>
+				<li>Blog</li>
+				<li>Jobs</li>
+				<li>Press</li>
+				<li>Contact Us</li>
+			</ul>
+		</div>
+		<div>
+			<h6>Blog</h6>
+			<ul>
+				<li>Startup Journey</li>
+				<li>August 13, 2014</li>
+				<li>Site Redesign</li>
+				<li>August 26, 2014</li>
+			</ul>
+		</div>
+		<div>
+			<h6>Connect</h6>
+			<h5>Join our mailing list</h5>
+		</div>
+	</div>
+</footer>
 </body>
 </html>
